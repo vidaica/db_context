2e9a633ff88b85e33920829a1bbc9552d5028be7
Vi Dang <dangquocvi@gmail.com>
2015-03-16 10:18:08 +0700

    documentation for Array#make_{association_name}

--- README.md
+++ README.md
@@ -1,6 +1,21 @@
 # DbContext
 
-TODO: Write a gem description
+This gem is an extension to factory_girl gem. It is supposed to help write more readble test code and improve test performance with bulk insertion when neccessary.
+
+Without db-context, to create an author with three associate posts, each post has three comments
+  
+    author = FactoryGirl.create :author
+    
+    3.times.map do
+      FactoryGirl.create :post, author: author
+    end
+    .each do |post|
+      3.times{ FactoryGirl.create :comment, post: post }
+    end    
+    
+With db-context, we can do the same with much less and readable code
+  
+    1.Author.has_3_posts(:assoc).each_has_3_comments
 
 ## Installation
 
@@ -18,7 +33,75 @@ Or install it yourself as:
 
 ## Usage
 
-TODO: Write usage instructions here
+### Extensions to Array <a name='array'></a>
+
+####\#belongs\_to\_{association_name}(associate\_objects, *directives, &block)
+
+    class Post
+      belongs_to :writer, class_name: 'Author', foreign_key: 'author_id'
+    end
+    class Author
+      has_many :posts
+    end
+    
+    6.Posts.belongs_to_writer 3.Authors # 6 posts will be allocated equally for 3 authors
+    
+    # the associate name can be omitted from the method call if it can be elicited from associated class
+    class Post
+      belongs_to :author
+    end
+    
+    6.Posts.belongs_to 3.Authors # :author association can be elicited from Author class
+    
+    # if the items cannot be allocated equally to associated objects, extra objects are allocated randomly
+    
+    8.Posts.belongs_to 3.Authors # each author gets 2 posts, 2 remaining posts are allocated randomly
+    
+    # associate objects can be a single object instead of an array of associate objects
+    
+    3.Posts.belongs_to FactoryGirl.create :author
+    
+    # to return the associated objects instead of the caller, use :assoc directive
+    
+    6.Posts.belongs_to(3.Authors, :assoc) # the array of authors will be returned instead of the array of posts
+    
+    # with a code block, the block is executed before each pair of object are saved
+    
+    6.Posts.belongs_to 3.Authors do |post, author|
+      post.description = "This post is written by #{author.name}"
+    end
+    
+    # when the objects are saved to set up the association, validations are ignored
+    
+    6.Posts.belongs_to 3.Authors do |post, author|
+      author.email = "" # this will not cause an validation failure even though email is required
+    end
+
+####\#make_{association_name}(*directives, options = {}, &block)
+    
+    # Factory_girl will always be used to insert associate objects
+    3.Posts.make_author # create one author for each of three posts
+    
+    # With factory
+    3.Posts.make_author(factory: :valid_author) # create authors with :valid_author factory
+    
+    # With factory, trait and overiden attributes
+    3.Posts.make_author(factory: [:author, :valid, first_name: 'Josh', last_name: 'Smith' ])
+    
+    # With a block, the block will be executed just before objects are saved
+    3.Posts.make_author do |post, author_attributes|
+      author_attributes[:last_name] = 'Smith'
+      post.description = "Writen by #{author_attributes[:first_name]}"
+    end
+    
+    # With a block, validations will be ignored when existing objects are saved
+    3.Posts.make_author do |post, author_attributes|
+      author_attributes[:last_name] = '' # causes validation failure if last_name is required
+      post.description = "" # not cause validation failure even if description is required
+    end
+    
+    # To return the associate objects instead of the caller, use :assoc directive
+    3.Posts.make_author(:assoc) # array of newly created authors will be returned    
 
 ## Contributing
 
---
+++ db/database.sql
@@ -0,0 +1,154 @@
+-- MySQL dump 10.13  Distrib 5.5.41, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: db_context
+-- ------------------------------------------------------
+-- Server version	5.5.41-0ubuntu0.14.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `bosses`
+--
+CREATE DATABASE db_context;
+
+USE db_context;
+
+DROP TABLE IF EXISTS `bosses`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `bosses` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `bosses`
+--
+
+LOCK TABLES `bosses` WRITE;
+/*!40000 ALTER TABLE `bosses` DISABLE KEYS */;
+/*!40000 ALTER TABLE `bosses` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `children`
+--
+
+DROP TABLE IF EXISTS `children`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `children` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `father_id` int(11) DEFAULT NULL,
+  `foster_father_id` int(11) DEFAULT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `nickname` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `gender` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `children`
+--
+
+LOCK TABLES `children` WRITE;
+/*!40000 ALTER TABLE `children` DISABLE KEYS */;
+/*!40000 ALTER TABLE `children` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `fathers`
+--
+
+DROP TABLE IF EXISTS `fathers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `fathers` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `boss_id` int(11) DEFAULT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `nickname` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `complexion` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `number` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `fathers`
+--
+
+LOCK TABLES `fathers` WRITE;
+/*!40000 ALTER TABLE `fathers` DISABLE KEYS */;
+/*!40000 ALTER TABLE `fathers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `schema_migrations`
+--
+
+DROP TABLE IF EXISTS `schema_migrations`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `schema_migrations` (
+  `version` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  UNIQUE KEY `unique_schema_migrations` (`version`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `schema_migrations`
+--
+
+LOCK TABLES `schema_migrations` WRITE;
+/*!40000 ALTER TABLE `schema_migrations` DISABLE KEYS */;
+INSERT INTO `schema_migrations` VALUES ('20130723090511'),('20130723091524'),('20140703080525'),('20140704090917');
+/*!40000 ALTER TABLE `schema_migrations` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `toys`
+--
+
+DROP TABLE IF EXISTS `toys`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `toys` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `child_id` int(11) DEFAULT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `toys`
+--
+
+LOCK TABLES `toys` WRITE;
+/*!40000 ALTER TABLE `toys` DISABLE KEYS */;
+/*!40000 ALTER TABLE `toys` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2015-02-11 18:08:40
